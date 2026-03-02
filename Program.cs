cb424ee3fdb4811e2d4ef53a6d8b4791841a2544
Isamilmaher <ismailmaher6977@gmail.com>
2026-03-03 00:44:37 +0200

    Question 5

--- Program.cs
+++ Program.cs
@@ -133,60 +133,106 @@ namespace G_Net_34_CS04
             //Console.WriteLine(Weather);
             #region Question 6
 
-            int[] scores = { 85, 42, 91, 67, 55, 78, 39, 88, 72, 95, 60, 48 };
-            Console.WriteLine("Failing scores (below 50):");
-            foreach (int score in scores)
-            {
-                if (score < 50)
-                {
-                    Console.Write(score);
-                    Console.Write("/n");
-                }
-            }
-            Console.WriteLine("/n");
-            foreach (int score in scores)
-            {
-                if (score > 90)
-                {
-                    Console.WriteLine(score);
-                    break;
-                }
-            }
-            int sum = 0;
-            int count = 0;
-            foreach (int score in scores)
-            {
-                if (score >= 40)
-                {
-                    sum += score;
-                    count++;
-                }
-            }
-            double average = sum / count;
-            Console.WriteLine($"Class average (excluding absents)): {average}");
+            //int[] scores = { 85, 42, 91, 67, 55, 78, 39, 88, 72, 95, 60, 48 };
+            //Console.WriteLine("Failing scores (below 50):");
+            //foreach (int score in scores)
+            //{
+            //    if (score < 50)
+            //    {
+            //        Console.Write(score);
+            //        Console.Write("/n");
+            //    }
+            //}
+            //Console.WriteLine("/n");
+            //foreach (int score in scores)
+            //{
+            //    if (score > 90)
+            //    {
+            //        Console.WriteLine(score);
+            //        break;
+            //    }
+            //}
+            //int sum = 0;
+            //int count = 0;
+            //foreach (int score in scores)
+            //{
+            //    if (score >= 40)
+            //    {
+            //        sum += score;
+            //        count++;
+            //    }
+            //}
+            //double average = sum / count;
+            //Console.WriteLine($"Class average (excluding absents)): {average}");
 
 
-            int gradeA = 0, gradeB = 0, gradeC = 0, gradeD = 0, gradeF = 0;
+            //int gradeA = 0, gradeB = 0, gradeC = 0, gradeD = 0, gradeF = 0;
 
-            foreach (int score in scores)
-            {
-                if (score >= 90) gradeA++;
-                else if (score >= 80) gradeB++;
-                else if (score >= 70) gradeC++;
-                else if (score >= 60) gradeD++;
-                else gradeF++;
-            }
+            //foreach (int score in scores)
+            //{
+            //    if (score >= 90) gradeA++;
+            //    else if (score >= 80) gradeB++;
+            //    else if (score >= 70) gradeC++;
+            //    else if (score >= 60) gradeD++;
+            //    else gradeF++;
+            //}
 
-            Console.WriteLine("Grade distribution:");
-            Console.WriteLine("A: " + gradeA);
-            Console.WriteLine("B: " + gradeB);
-            Console.WriteLine("C: " + gradeC);
-            Console.WriteLine("D: " + gradeD);
-            Console.WriteLine("F: " + gradeF);
+            //Console.WriteLine("Grade distribution:");
+            //Console.WriteLine("A: " + gradeA);
+            //Console.WriteLine("B: " + gradeB);
+            //Console.WriteLine("C: " + gradeC);
+            //Console.WriteLine("D: " + gradeD);
+            //Console.WriteLine("F: " + gradeF);
 
             #endregion
             #endregion
 
+            #region Question 5
+            int attempts = 0;
+            const int Attempts = 5;
+            bool valid = false;
+
+            do
+            {
+                Console.WriteLine("Enter Your Password: ");
+                string password = Console.ReadLine();
+                attempts++;
+
+                bool hasUpper = false, hasDigit = false, hasSpece = false;
+                foreach (char c in password)
+                {
+                    if (char.IsUpper(c)) hasUpper = true;
+                    if (char.IsDigit(c)) hasDigit = true;
+                    if (char.IsWhiteSpace(c)) hasSpece = true;
+                }
+                string errors = "";
+                if (password.Length < 8) errors += "Password must be at least 8 \n";
+                if (!hasUpper) errors += "Password must contain at least one uppercase letter \n";
+                if (!hasDigit) errors += "Password must contain at least one digit \n";
+                if (hasSpece) errors += "Password must not contain spaces \n";
+
+                if (errors == "")
+                {
+                    valid = true;
+                    Console.WriteLine("Password is valid!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid password:\n" + errors);
+                    if (attempts >= Attempts)
+                    {
+                        Console.WriteLine("Maximum attempts reached. Access denied.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Attempt {attempts} of {Attempts}. Please try again.");
+                    }
+                }
+
+            } while (!valid);
+
+            #endregion
 
 
             #endregion
